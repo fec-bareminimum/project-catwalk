f01f8018b13d894e4d497669778d06980a835fd5
andy-ch3n <adch3n@gmail.com>
2021-09-10 17:22:03 -0700

    added some tests for product

---
+++ client/src/Components/Product/Product.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import App from './App.jsx';
+// import '@testing-library/jest-dom';
+
+
+describe('Product', () => {
+  test('renders Product component', () => {
+    render(<Product />);
+  });
+});
+
+describe('Product Title', () => {
+  test('Finds the Project Title', () => {
+    render(<Product />);
+    expect(screen.getByRole('header')).toBeInTheDocument()
+  });
+});
+
+describe('Product Price', () => {
+  test('Finds the price, () => {
+    render(<Product />);
+    expect(screen.getByText('price')).toBeInTheDocument()
+  });
+});
+
+
+describe('Ratings', () => {
+  test('Ratings', () => {
+    render(<Product />);
+    expect(screen.getByText('ratings')).toBeInTheDocument()
+  });
+});
+
+
+describe('Cart exists', () => {
+  test('Finds the cart', () => {
+    render(<Product />);
+    expect(screen.getByRole('Cart')).toBeInTheDocument() ||  expect(screen.getByRole('Bag')).toBeInTheDocument()
+  });
+});
+
+describe('Quantity selector works', () => {
+  test('Quantity selector works', () => {
+    render(<Product />);
+    expect(screen.getByText('button')).toBeInTheDocument()
+  });
+});
+
+// test('General Information displayed at top of Overview'), () => {
+//   expect()
+// }
+
