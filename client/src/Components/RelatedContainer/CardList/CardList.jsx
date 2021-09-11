7fb807c94242cb7c9507c9cd76e64cd5ca78f78c
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-10 20:29:13 -0700

    add working bootstrap styles with cdn

--- client/src/Components/RelatedContainer/CardList/CardList.jsx
+++ client/src/Components/RelatedContainer/CardList/CardList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductCard from './ProductCard/ProductCard';
+import ProductCard from './ProductCard/ProductCard.jsx';
 
 const CardList = () => {
   // accepts a list of products
