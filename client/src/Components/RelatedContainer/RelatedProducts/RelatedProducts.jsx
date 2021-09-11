9d5eb6a874119046a4d24bfe8db111532c016e74
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-10 17:10:36 -0700

    skeleton for related products section

---
+++ client/src/Components/RelatedContainer/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardList from '../CardList/CardList';
+
+const RelatedProducts = () => {
+  // will access the state for products in PRODUCTS
+  // will render the card list for that array
+
+  return (
+    <div className="relatedProductsContainer">
+      <p>under construction</p>
+      <CardList />
+    </div>
+  );
+};
+
+export default RelatedProducts;
