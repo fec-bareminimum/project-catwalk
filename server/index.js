e005e84d24a87219c585fbb8e9e9b1a525b19807
gab <guogabbi@gmail.com>
2021-09-10 19:55:50 -0700

    installed webpack v4 in place of v5

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const config = require('../config');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 
 app.get('/*', (req, res) => {
