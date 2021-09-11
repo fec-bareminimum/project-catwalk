e74e10abd60249cd536cf93965438ffeee5634c6
gab <guogabbi@gmail.com>
2021-09-10 20:16:15 -0700

    used bootstrap CSS CDN instead of importingfile

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 ReactDOM.render(<App />, document.getElementById('app'));
