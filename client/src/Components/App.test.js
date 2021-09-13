c87bf7a9c07d016107351b3587e03c6936471924
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-13 13:03:08 -0700

    add test-utils and refactor assertion

--- client/src/Components/App.test.js
+++ client/src/Components/App.test.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { render } from '../test-utils.jsx';
 import App from './App.jsx';
-// import '@testing-library/jest-dom';
-
-
-
 
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
   });
 });
-
-
-// test('General Information displayed at top of Overview'), () => {
-//   expect()
-// }
-
