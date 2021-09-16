85cd9ab448cee4755443f33ff8166fc219d80126
Spencer Lepine <spencerlepine26@gmail.com>
2021-09-16 09:47:56 -0700

    remove StarButton from branch

--- client/src/Components/RelatedContainer/RelatedProducts/StarButton/StarButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-// import useProducts from "../../../../contexts/ProductsContext.jsx"
-// import StarIcon from "./StarIcon.jsx"
-
-const StartButton = ({ thisProduct, displayedProduct }) => (
-  <button className="actionBtn">{/* <StarIcon /> */}</button>
-)
-
-export default StartButton
