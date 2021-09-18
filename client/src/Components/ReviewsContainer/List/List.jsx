15791a6c5c7777915635e1709123997eb80f531a
gab <guogabbi@gmail.com>
2021-09-18 14:58:25 -0700

    minor edits to ReviewList.jsx and List.jsx

--- client/src/Components/ReviewsContainer/List/List.jsx
+++ client/src/Components/ReviewsContainer/List/List.jsx
@@ -1,13 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 // import Sorter from "./Sorter.jsx"
 import ReviewList from "./ReviewList.jsx"
 // import Buttons from "./Buttons.jsx"
+import useReviews from "../../../contexts/ReviewsContext.jsx"
 
 const List = () => {
+  const { reviews, fetchReviews } = useReviews()
+  const [page, setPage] = useState(1) // based on list length/scroll
+  const [count, setCount] = useState(2) // based on list length/scroll
+  const [sort, setSort] = useState("relevant") // based on sort
+  const [product_id, setProduct_id] = useState(42367)
+
+  // console.log("reviews before", reviews)
+
+  useEffect(() => {
+    console.log("inside effect")
+    fetchReviews(page, count, sort, product_id, (data) => {
+      // console.log("reviews after", data)
+    })
+  }, [page, count, sort, product_id])
   return (
     <section className="list">
       {/* <Sorter /> */}
-      <ReviewList />
+      <ReviewList data={reviews} />
       {/* <Buttons /> */}
     </section>
   )
--- client/src/Components/ReviewsContainer/List/ReviewList.jsx
+++ client/src/Components/ReviewsContainer/List/ReviewList.jsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import CardGroup from "react-bootstrap/CardGroup"
 import ReviewTile from "./ReviewTile.jsx"
-import useReviews from "../../../contexts/ReviewsContext.jsx"
 
-const ReviewList = () => {
-  const { reviews, fetchReviews } = useReviews()
-  const page = 1 // based on list length/scroll
-  const count = 2 // based on list length/scroll
-  const sort = "relevant" // "newest", "helpful" based on sort
-  const product_id = 42367
-
-  useEffect(
-    (page, count, sort) => {
-      fetchReviews(page, count, sort, product_id, (data) => {
-        console.log(data)
-      })
-    },
-    [product_id]
-  )
-
+const ReviewList = (props) => {
   return (
     <CardGroup className="reviewList">
-      {reviews.map((review) => (
+      {props.data.map((review) => (
         <ReviewTile key={review.id} {...review} />
       ))}
     </CardGroup>
