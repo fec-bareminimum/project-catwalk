29cf14a77f47969938e28d07529fbbb8bfc4d687
gab <guogabbi@gmail.com>
2021-09-18 17:06:52 -0700

    added star ratings to ReviewTile.jsx

--- client/src/Components/ReviewsContainer/List/ReviewTile.jsx
+++ client/src/Components/ReviewsContainer/List/ReviewTile.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Card from "react-bootstrap/Card"
+import StarRatings from "react-star-ratings"
 
 const ReviewTile = (props) => {
-  const stars = "null"
   const helpful = "null"
 
   return (
     <Card style={{ width: "18rem" }} className="reviewTile">
-      <Card.Header>{stars}</Card.Header>
+      <Card.Header>
+        <StarRatings rating={props.rating} starDimension="10px" starSpacing="0" />
+      </Card.Header>
       <Card.Body>
         <Card.Title>{props.summary}</Card.Title>
         <Card.Text>{props.body}</Card.Text>
