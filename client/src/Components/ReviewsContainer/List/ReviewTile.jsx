68b16b749aa2da55bcfba684159239faac0be463
gab <guogabbi@gmail.com>
2021-09-18 19:03:25 -0700

    installed moment for dates on ReviewTile(s)

--- client/src/Components/ReviewsContainer/List/ReviewTile.jsx
+++ client/src/Components/ReviewsContainer/List/ReviewTile.jsx
@@ -8,6 +8,25 @@ const ReviewTile = (props) => {
   const [helpful, setHelpful] = useState(false)
   const [report, setReport] = useState(false)
 
+  const stars = (
+    <StarRatings
+      className="stars"
+      rating={props.rating}
+      starDimension="15px"
+      starSpacing="0"
+    />
+  )
+
+  const date = () => {
+    return new Intl.DateTimeFormat().format(props.date)
+  }
+
+  const recommend = () => {
+    if (props.recommend) {
+      return <Card.Text className="recommend">I recommend this product!</Card.Text>
+    }
+  }
+
   const handleHelpful = () => {
     if (!helpful) {
       markReviewHelpful(props.review_id)
@@ -23,18 +42,25 @@ const ReviewTile = (props) => {
   }
 
   return (
-    <Card style={{ width: "18rem" }} className="reviewTile">
+    <Card className="reviewTile">
       <Card.Header>
-        <StarRatings rating={props.rating} starDimension="15px" starSpacing="0" />
+        {stars}
+        {props.reviewer_name}, {date()}
       </Card.Header>
       <Card.Body>
         <Card.Title>{props.summary}</Card.Title>
         <Card.Text>{props.body}</Card.Text>
+        {recommend()}
       </Card.Body>
       <Card.Footer>
-        Helpful?
-        <Card.Link onClick={handleHelpful}>Yes ({props.helpfulness})</Card.Link>|
-        <Card.Link onClick={handleReport}>Report</Card.Link>
+        Helpful?{" "}
+        <Card.Link className="helpful" onClick={handleHelpful}>
+          Yes ({props.helpfulness})
+        </Card.Link>{" "}
+        |
+        <Card.Link className="report" onClick={handleReport}>
+          Report
+        </Card.Link>
       </Card.Footer>
     </Card>
   )
