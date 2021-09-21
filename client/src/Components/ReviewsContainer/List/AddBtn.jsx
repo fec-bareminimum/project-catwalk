3be6bcd31981ea9381567dd85b42a5941dd7f7b7
gab <guogabbi@gmail.com>
2021-09-21 03:13:16 -0700

    added detail text to Overall rating

--- client/src/Components/ReviewsContainer/List/AddBtn.jsx
+++ client/src/Components/ReviewsContainer/List/AddBtn.jsx
@@ -10,43 +10,62 @@ import useReviews from "../../../contexts/ReviewsContext.jsx"
 
 const AddBtn = (props) => {
   const { reviews, productInfo } = useReviews()
+  const [show, setShow] = useState(false)
 
-  const AddBtn = () => {
-    const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const Overall = () => {
     const [stars, setStars] = useState(0)
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
+    const [details, setDetails] = useState({
+      5: "Great",
+      4: "Good",
+      3: "Average",
+      2: "Fair",
+      1: "Poor",
+    })
+
     return (
-      <Col>
-        <Button className="add" onClick={handleShow} style={{ float: "right" }}>
-          Add a review
-        </Button>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton></Modal.Header>
-          <Modal.Body>
-            <Form>
-              <h3>Write Your Review</h3>
-              {/* <h5>{`About the ${productInfo.name}`}</h5> */}
-              <h5>About the Camo Onesie</h5>
-              <Col>
-                <Form.Label>Overall rating*</Form.Label>
-              </Col>
-              <Col>
-                <StarRatings
-                  rating={stars}
-                  changeRating={(rating) => setStars(rating)}
-                  starDimension="15px"
-                  starSpacing="0"
-                />
-              </Col>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </Col>
+      <Container>
+        <Col>
+          <Form.Label>Overall rating*</Form.Label>
+        </Col>
+        <Col>
+          <StarRatings
+            rating={stars}
+            changeRating={(rating) => setStars(rating)}
+            starDimension="15px"
+            starSpacing="0"
+          />
+          {stars > 0 ? <Form.Text>{details[stars]}</Form.Text> : null}
+        </Col>
+      </Container>
     )
   }
 
-  return <Col>{reviews ? <AddBtn /> : null}</Col>
+  return (
+    <Col>
+      {/* {productInfo ? (*/}
+      {reviews ? (
+        <Col>
+          <Button className="add" onClick={handleShow} style={{ float: "right" }}>
+            Add a review
+          </Button>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+              <h3>Write Your Review</h3>
+              {/* <h5>{`About the ${productInfo.name}`}</h5> */}
+              <h5>About the Camo Onesie</h5>
+              <Form>
+                <Overall />
+              </Form>
+            </Modal.Body>
+          </Modal>
+        </Col>
+      ) : null}
+    </Col>
+  )
 }
 
 export default AddBtn
