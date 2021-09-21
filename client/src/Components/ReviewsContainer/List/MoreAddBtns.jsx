af2427cd3fbb70a24d23167579de1f02a5cda4b7
gab <guogabbi@gmail.com>
2021-09-21 01:22:19 -0700

    set up AddBtn with Modal in List/MoreAddBtn

--- client/src/Components/ReviewsContainer/List/MoreAddBtns.jsx
+++ client/src/Components/ReviewsContainer/List/MoreAddBtns.jsx
@@ -1,33 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
 import useReviews from "../../../contexts/ReviewsContext.jsx"
 
 const MoreAddBtns = (props) => {
   const { reviews } = useReviews()
 
   const MoreBtn = () => (
-    <Button
-      className="more"
-      onClick={() => {
-        props.showMore()
-      }}
-    >
-      More Reviews
-    </Button>
+    <Col>
+      <Button
+        className="more"
+        onClick={() => {
+          props.showMore()
+        }}
+        style={{ float: "left" }}
+      >
+        More reviews
+      </Button>
+    </Col>
   )
+
+  const AddBtn = () => {
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+    return (
+      <Col>
+        <Button className="add" onClick={handleShow} style={{ float: "right" }}>
+          Add a review
+        </Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton></Modal.Header>
+          <Modal.Body></Modal.Body>
+        </Modal>
+      </Col>
+    )
+  }
 
   return (
     <Container className="buttons">
       <Row>
-        <Col>
-          {reviews && props.listLength < reviews.length ? <MoreBtn /> : null}
-        </Col>
-        {/* <Col>
-            <Button className="add">Add a Review</Button>
-          </Col> */}
+        {reviews && props.listLength < reviews.length ? <MoreBtn /> : null}
+        <AddBtn />
       </Row>
     </Container>
   )
